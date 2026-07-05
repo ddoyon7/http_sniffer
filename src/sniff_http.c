2bd55fc2e7b0acfedfad51babfc016f24e4e29bf
ddoyon7 <ddoyon@korea.ac.kr>
2026-07-05 16:10:47 +0900

    Edit comment

--- src/sniff_http.c
+++ src/sniff_http.c
@@ -7,7 +7,7 @@
 
 void got_packet(u_char* args, const struct pcap_pkthdr* header,
                 const u_char* packet) {
-  // Ehternet header : src mac / dst mac
+  // Ethernet header : src mac / dst mac
   struct ethheader* eth = (struct ethheader*)packet;
 
   printf("=======Ethernet Header=======\n");
